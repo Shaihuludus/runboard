e2ec4c5cd9173275ec218e7afda9373d8d3a16bb
Daniel Madejek <dmadejek@gmail.com>
2017-06-01 21:49:39 +0200

    css

--- src/main/kotlin/pl/madshai/runboard/RunBoardFx.kt
+++ src/main/kotlin/pl/madshai/runboard/RunBoardFx.kt
@@ -5,7 +5,9 @@ import javafx.application.Platform
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
+import javafx.scene.paint.Color
 import javafx.stage.Stage
+import javafx.stage.StageStyle
 import pl.madshai.runboard.views.TrayIconController
 import java.awt.Toolkit.getDefaultToolkit
 import java.awt.SystemTray
@@ -23,10 +25,12 @@ class RunBoardFx : Application() {
     override fun start(primaryStage: Stage) {
         Platform.setImplicitExit(false);
         val root: Parent = FXMLLoader.load(javaClass.classLoader.getResource("RunBoard.fxml"));
-        val scene = Scene(root, 300.0, 275.0);
+        var scene = Scene(root);
+        scene.fill = Color.TRANSPARENT;
         val controller = TrayIconController(primaryStage);
         primaryStage.title = "Fxml Welcome";
         primaryStage.scene = scene;
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.show();
     }
 
