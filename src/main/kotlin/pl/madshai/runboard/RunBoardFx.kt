bd00204abc652ffb6a8da977885d8ab29ca23be9
Daniel Madejek <daniel.madejek@cognifide.com>
2017-06-02 13:07:11 +0200

    tornado fx and working ui

--- src/main/kotlin/pl/madshai/runboard/RunBoardFx.kt
+++ src/main/kotlin/pl/madshai/runboard/RunBoardFx.kt
@@ -1,18 +1,16 @@
 package pl.madshai.runboard
 
-import javafx.application.Application
 import javafx.application.Platform
-import javafx.fxml.FXMLLoader
-import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.scene.paint.Color
 import javafx.stage.Stage
 import javafx.stage.StageStyle
-import pl.madshai.runboard.views.TrayIconController
-import java.awt.Toolkit.getDefaultToolkit
-import java.awt.SystemTray
-import java.awt.Toolkit
+import pl.madshai.runboard.views.MainBoardView
+import tornadofx.*
+import java.awt.MenuItem
+import java.awt.PopupMenu
 import java.awt.TrayIcon
+import java.awt.event.MouseEvent
 import javax.imageio.ImageIO
 
 
@@ -20,18 +18,58 @@ import javax.imageio.ImageIO
  * Created by daniel.madejek on 2017-06-01.
  */
 
-class RunBoardFx : Application() {
+class RunBoardFx : App(MainBoardView::class) {
+
+
+    private var stageControls = MainBoardData();
+
 
     override fun start(primaryStage: Stage) {
-        Platform.setImplicitExit(false);
-        val root: Parent = FXMLLoader.load(javaClass.classLoader.getResource("RunBoard.fxml"));
-        var scene = Scene(root);
-        scene.fill = Color.TRANSPARENT;
-        val controller = TrayIconController(primaryStage);
-        primaryStage.title = "Fxml Welcome";
-        primaryStage.scene = scene;
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
-        primaryStage.show();
+        this.stageControls.stage = primaryStage;
+        prepareTrayMenu()
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        super.start(primaryStage)
     }
 
+    override fun createPrimaryScene(view: UIComponent): Scene {
+        var toReturn = Scene(view.root)
+        toReturn.fill = Color.TRANSPARENT
+        return toReturn
+    }
+
+    private fun prepareTrayMenu() {
+        trayicon(ImageIO.read(ClassLoader.getSystemResource("GameCenter-icon.png")), "RunBoard", false, false, { configureTray(this) })
+    }
+
+    private fun configureTray(tray: TrayIcon) {
+        tray.setOnMouseClicked(true, MouseEvent.BUTTON1, 1, {
+            showMenuChange()
+            stageShow()
+        })
+        tray.menu("RunBoard", { createItems(this) })
+    }
+
+    private fun stageShow() {
+        if (this.stageControls.stage.isShowing) {
+            this.stageControls.stage.close()
+        } else {
+            this.stageControls.stage.show()
+        }
+    }
+
+    private fun showMenuChange() {
+        if (this.stageControls.stage.isShowing) {
+            this.stageControls.showMenuItem.label = "Show"
+        } else {
+            this.stageControls.showMenuItem.label = "Hide"
+        }
+
+    }
+
+    private fun createItems(popupMenu: PopupMenu) {
+        this.stageControls.showMenuItem = popupMenu.item("Hide", null, { this.setOnAction(true, { showMenuChange(); stageShow() }) })
+        popupMenu.item("Exit", null, { this.setOnAction { Platform.exit() } })
+    }
 }
+
+data class MainBoardData(var stage: Stage = Stage(), var showMenuItem: MenuItem = MenuItem())
---
+++ src/main/kotlin/pl/madshai/runboard/views/MainBoardView.kt
@@ -0,0 +1,13 @@
+package pl.madshai.runboard.views
+
+import javafx.scene.Parent
+import tornadofx.*
+
+/**
+ * Created by daniel.madejek on 2017-06-02.
+ */
+
+class MainBoardView : View(){
+    override val root: Parent by fxml("/views/RunBoard.fxml");
+
+}
